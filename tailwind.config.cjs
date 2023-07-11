fc14f1ecb72ee03c4342dbf22ac7e0d06ad04e43
Canaris <mrgeek484@gmail.com>
2023-07-11 17:52:04 +0800

    e

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans],
+			},
+			screens: {
+				'2xs': '320px',
+				'xs': '475px',
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/typography')],
 };
