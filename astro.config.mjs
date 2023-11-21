d8abfa20e5e0f766ee6f74a0e56f805108e4afa0
Canaris <mrgeek484@gmail.com>
2023-11-21 14:42:24 +0800

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,9 @@ import sentry from '@sentry/astro';
 import node from '@astrojs/node';
 import { loadEnv } from 'vite';
 import vercel from '@astrojs/vercel/serverless';
-
-
+import critters from 'astro-critters';
 const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hcihsc.pages.dev',
@@ -23,33 +23,25 @@ export default defineConfig({
 		prefetchAll: true,
 		defaultStrategy: 'viewport',
 	},
-	integrations: [
-		react(),
-		tailwind(),
-		mdx(),
-		sitemap(),
-		icon({
-			include: {
-				mdi: ['calendar', 'location', 'facebook', 'instagram', 'youtube', 'arrow-right'],
-				uil: ['envelope', 'instagram'],
-			},
-		}),
-		partytown({
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
-		sentry({
-			dsn: 'https://7fe022e75fa6bbf1ba04f227f37431c9@o975437.ingest.sentry.io/4506211627565056',
-			sourceMapsUploadOptions: {
-				project: 'hci-hsc',
-				authToken: env.SENTRY_AUTH_TOKEN,
-			},
-		}),
-	],
+	integrations: [react(), tailwind(), mdx(), sitemap(), icon({
+		include: {
+			mdi: ['calendar', 'location', 'facebook', 'instagram', 'youtube', 'arrow-right'],
+			uil: ['envelope', 'instagram'],
+		},
+	}), partytown({
+		config: {
+			forward: ['dataLayer.push'],
+		},
+	}), sentry({
+		dsn: 'https://7fe022e75fa6bbf1ba04f227f37431c9@o975437.ingest.sentry.io/4506211627565056',
+		sourceMapsUploadOptions: {
+			project: 'hci-hsc',
+			authToken: env.SENTRY_AUTH_TOKEN,
+		},
+	}), critters()],
 	output: 'server',
 	// adapter: node({ mode: 'standalone' }),
 	adapter: vercel({
-		// imageService: true,
+		imageService: true,
 	}), // kys cloudflare
 });
