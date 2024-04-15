3c18e6e7f5d05c39654590c6e6532cdcfbd98ceb
Canaris <mrgeek484@gmail.com>
2024-04-15 21:17:36 +0800

    idk

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,4 +1,3 @@
-import type { CollectionEntry } from 'astro:content';
 import { Icon } from '@iconify/react';
 import dayjs from 'dayjs';
 import { Listbox, Transition } from '@headlessui/react';
@@ -7,8 +6,69 @@ import { useState, Fragment } from 'react';
 import type React from 'react';
 import Fuse from 'fuse.js';
 import type { Events as EventsType } from '@lib/contentful';
+import { Chrono } from 'react-chrono';
+import './Events.css';
+
 
 export default function Events({ events }: { events: EventsType[] }) {
+	const [tabIndex, setTabIndex] = useState(0);
+
+	return (
+		<>
+			<div className='flex justify-center items-center space-x-4 mt-8'>
+				<button onClick={() => setTabIndex(0)} className={`px-4 py-2 rounded-lg focus:outline-none ${tabIndex === 0 ? 'bg-amber-600 text-white' : 'bg-gray-200 text-gray-700'}`}>Events</button>
+				<button onClick={() => setTabIndex(1)} className={`px-4 py-2 rounded-lg focus:outline-none ${tabIndex === 1 ? 'bg-amber-600 text-white' : 'bg-gray-200 text-gray-700'}`}>Timeline</button>
+			</div>
+
+			{tabIndex === 0 ? <EventsSearch events={events} /> : <Timeline events={events} />}
+		</>
+	);
+}
+
+
+function Timeline({ events }: { events: EventsType[] }) {
+	/*
+	const items = [{
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg"
+        }
+      }
+    }, ...];
+	*/
+	return (
+		<div className='mx-auto mt-12'>
+			<Chrono
+				items={events.map((event) => ({
+					title: dayjs(event.fields.date as unknown as string).format('D MMM YYYY'),
+					cardTitle: event.fields.title as unknown as string,
+					cardSubtitle: event.fields.location as unknown as string,
+					cardDetailedText: event.fields.description as unknown as string,
+					media: {
+						type: 'IMAGE',
+						source: {
+							url: event.fields.banner.fields.file.url,
+						},
+					},
+				}))}
+				mode='VERTICAL_ALTERNATING'
+				enableDarkToggle={true}
+				textOverlay
+				cardHeight={350}
+				disableToolbar={true}
+			/>
+		</div>
+	);
+}
+
+
+function EventsSearch({ events }: { events: EventsType[] }) {
 	const options = [
 		{
 			name: 'Upcoming',
@@ -67,7 +127,7 @@ export default function Events({ events }: { events: EventsType[] }) {
 										key={personIdx}
 										className={({ active }) =>
 											`relative cursor-default select-none py-2 pl-10 pr-4 ${
-												active ? 'bg-amber-100 text-amber-900' : 'text-gray-700'
+												active ? 'bg-amber</div>-100 text-amber-900' : 'text-gray-700'
 											}`
 										}
 										value={person}
@@ -100,24 +160,26 @@ export default function Events({ events }: { events: EventsType[] }) {
 				{results.filter(filter.filter).length > 0 ? results
 					.filter(filter.filter)
 					.map((event) => (
-						<div className='bg-gray-200 rounded-lg shadow-2xl overflow-hidden' key={event.fields.slug}>
-							<img className='w-full h-48 object-cover' src={event.fields.banner.fields.file.url} alt={event.fields.banner.fields.file.title} />
-							<div className='p-4'>
-								<div className='text-lg font-semibold text-gray-700'>{event.fields.title as unknown as string}</div>
-								<div className='mt-2 text-sm text-gray-500 flex items-center space-x-1'>
-									<Icon icon='mdi:calendar' className="w-4 h-4 inline-block" />
-									<span>{dayjs(event.fields.date as unknown as string).format('D MMM YYYY hh:mm a')}</span>
+						<>
+							<div className='bg-gray-200 rounded-lg shadow-2xl overflow-hidden' key={event.fields.slug}>
+								<img className='w-full h-48 object-cover' src={event.fields.banner.fields.file.url} alt={event.fields.banner.fields.file.title} />
+								<div className='p-4'>
+									<div className='text-lg font-semibold text-gray-700'>{event.fields.title as unknown as string}</div>
+									<div className='mt-2 text-sm text-gray-500 flex items-center space-x-1'>
+										<Icon icon='mdi:calendar' className="w-4 h-4 inline-block" />
+										<span>{dayjs(event.fields.date as unknown as string).format('D MMM YYYY hh:mm a')}</span>
+									</div>
+									{event.fields.location && <div className='mt-2 text-sm text-gray-500 flex items-center space-x-1'>
+										<Icon icon='mdi:location' className="w-4 h-4 inline-block" />
+										<span>{event.fields.location as unknown as string}</span>
+									</div>}
+									<div className='mt-2 text-sm text-gray-500 line-clamp-2'>{event.fields.description as unknown as string}</div>
+									{event.fields.link && (<div className='mt-2 text-sm text-gray-500'>
+										<a href={event.fields.link as unknown as string} target='_blank' rel='noopener noreferrer'>{event.fields.link as unknown as string}</a>
+									</div>)}
 								</div>
-								{event.fields.location && <div className='mt-2 text-sm text-gray-500 flex items-center space-x-1'>
-									<Icon icon='mdi:location' className="w-4 h-4 inline-block" />
-									<span>{event.fields.location as unknown as string}</span>
-								</div>}
-								<div className='mt-2 text-sm text-gray-500 line-clamp-2'>{event.fields.description as unknown as string}</div>
-								{event.fields.link && (<div className='mt-2 text-sm text-gray-500'>
-									<a href={event.fields.link as unknown as string} target='_blank' rel='noopener noreferrer'>{event.fields.link as unknown as string}</a>
-								</div>)}
 							</div>
-						</div>
+						</>
 					)) : (
 					<div className='flex flex-col items-center justify-center text-gray-500 col-span-full'>
 						<Icon icon='mdi:calendar' className='w-12 h-12' />
