41f395315a612a0baa785bcd6afc3b33b0a34401
Canaris <mrgeek484@gmail.com>
2024-07-21 15:28:58 +0800

    grid spacing

--- src/components/Initiatives.tsx
+++ src/components/Initiatives.tsx
@@ -35,7 +35,7 @@ export default function Initiatives({ initiatives }: { initiatives: InitiativesT
 					onChange={handleQuery}
 				/>
 			</div>
-			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6 mt-8'>
 				{filteredInitiatives.map((initiative) => (
 					<div className='rounded-lg bg-gray-200 shadow-2xl overflow-hidden' key={initiative.fields.slug as unknown as string}>
 						{initiative.fields.banner &&
